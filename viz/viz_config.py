b45f597519a6bde831372087edae290e66f927dc
Quoding <quoding@protonmail.ch>
2022-08-23 18:35:40 -0400

    Add viz code

---
+++ viz/viz_config.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = (7, 5)  # default = (6.4, 4.8)
+plt.rcParams["text.usetex"] = True
+plt.rcParams["figure.dpi"] = 140  # default = 100
+plt.rcParams["font.family"] = "serif"
+# plt.rcParams["text.latex.preamble"] = [r"\usepackage{amsmath}"]
+plt.style.use("ggplot")
+title_font_size = "10"
